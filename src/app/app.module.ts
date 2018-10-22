a35e101bed26512300257599ca198acc05358f30
Thales Souto <thaleshenriquepb@gmail.com>
2018-10-22 10:31:37 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule, MatFormFieldModule, MatButtonModule, MatIconModule, MatInputModule,
+        MatSlideToggleModule, MatListModule, MatCheckboxModule, MatProgressBarModule,
+        MatGridListModule, MatSelectModule, MatProgressSpinnerModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatCheckboxModule,
+    MatProgressBarModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
